792c4f8d4df5d9c6c7fb5a624fadf10822f669cc
lhk0705 <596113510@qq.com>
2020-10-19 16:47:24 +0800

    柱状图

--- src/components/overall/group/fstTest.vue
+++ src/components/overall/group/fstTest.vue
@@ -1,15 +1,46 @@
 <template>
-  <div>
-      
-  </div>
+  <ve-histogram 
+  :data="chartData"
+  :extend="extend"
+  ></ve-histogram>
 </template>
 
 <script>
-export default {
+  export default {
+    data () {
+      return {
+      extend:{
+        grid:{
+          top:"130px"
+        },
+        title:{
+          text:"本月首轮验收通过率",
+          left:"center"
+        },
+        legend:{
+          top:"40px"
+        }
+      },
+        chartData: {
+          columns: ['group', 'OA系统组','人力系统组','规划建设组','技术研发组','平台支撑组','内部支撑组'],
+          rows: [
+            { 'group': '' ,
+            'OA系统组': '0.8', 
+            '人力系统组':'0.9' ,
+            '规划建设组':'0.9' ,
+            '技术研发组':'0.9' ,
+            '平台支撑组':'0.9' ,
+            '内部支撑组':'0.9' ,},  
+          ]
+        }
+      }
+    },
+    created(){
 
-}
+    }
+  }
 </script>
 
-<style>
+<style scoped>
 
 </style>
--- src/components/overall/group/randomTest.vue
+++ src/components/overall/group/randomTest.vue
@@ -1,28 +1,46 @@
 <template>
-  <ve-histogram :data="chartData"></ve-histogram>
+  <ve-histogram 
+  :data="chartData"
+  :extend="extend"
+  ></ve-histogram>
 </template>
 
 <script>
   export default {
     data () {
       return {
+      extend:{
+        grid:{
+          top:"130px"
+        },
+        title:{
+          text:"本月抽测通过率",
+          left:"center"
+        },
+        legend:{
+          top:"40px"
+        }
+      },
         chartData: {
-          columns: ['group', 'month','pass'],
+          columns: ['group', 'OA系统组','人力系统组','规划建设组','技术研发组','平台支撑组','内部支撑组'],
           rows: [
-            { 'group': 'OA系统组', 'month': '9月', 'pass':'0.9' },
-            { 'group': '人力系统组', 'month': '9月', 'pass':'0.9' },
-            { 'group': '人力系统组', 'month': '9月', 'pass':'0.9'},
-            { 'group': '人力系统组', 'month': '9月', 'pass':'0.9'},
-            { 'group': '人力系统组', 'month': '9月', 'pass':'0.9' },
-            { 'group': '人力系统组', 'month': '9月', 'pass':'0.9'},
-            
+            { 'group': '' ,
+            'OA系统组': '0.8', 
+            '人力系统组':'0.9' ,
+            '规划建设组':'0.9' ,
+            '技术研发组':'0.9' ,
+            '平台支撑组':'0.9' ,
+            '内部支撑组':'0.9' ,},  
           ]
         }
       }
+    },
+    created(){
+
     }
   }
 </script>
 
-<style>
+<style scoped>
 
 </style>
