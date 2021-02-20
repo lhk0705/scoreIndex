fb5bc96551416e42d781acfd94ebffa755273a6c
lhk0705 <596113510@qq.com>
2021-02-20 16:07:13 +0800

    1

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ Vue.config.productionTip = false;
 import axios from 'axios';
 
 // axios.defaults.baseURL='http://192.168.128.247:8088'
-axios.defaults.baseURL='http://172.21.54.86:8088'
+axios.defaults.baseURL='http://172.21.54.86:8088/api/'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
